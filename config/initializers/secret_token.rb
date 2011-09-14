d1bd849a72db4320101921956809d7cc85efd9b2
Jose Diaz <jose.a.diaz@aguaseca.com>
2011-09-13 23:56:23 -0700

    initial commit after command: rails new iQ -O -j -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IQ::Application.config.secret_token = 'c7588305ea30363ddd42d4c8a907780706ec15fe3566aca1ab834855dcdcbe424762f2b1fea8167862cecba12d0fcc9a7a8ecd440be475d16b8cf33c3fa67fb4'

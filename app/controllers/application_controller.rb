cbb7352c91af5ab66c3a6655c62ce3b835365a6c
Jose Diaz <jose.a.diaz@aguaseca.com>
2011-12-14 01:45:37 -0700

    now redirects to sign in instead of root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  protected
+
+  def not_authenticated
+    redirect_to sign_in_path, :alert => "Please sign in first."
+  end
+
 end
--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -7,7 +7,7 @@ Rails.application.config.sorcery.submodules = []
 # Here you can configure each submodule's features.
 Rails.application.config.sorcery.configure do |config|
   # -- core --
-  # config.not_authenticated_action = :not_authenticated              # what controller action to call for
+  config.not_authenticated_action = :not_authenticated                # what controller action to call for
                                                                       # non-authenticated users. 
                                                                       # You can also override 'not_authenticated' 
                                                                       # instead.
